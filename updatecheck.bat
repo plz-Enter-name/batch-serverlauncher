176add416eb1c6e7ea3cffe47c56f9c0161b65f4
plz-Enter-name <104212145+plz-Enter-name@users.noreply.github.com>
2022-05-02 11:18:25 +0900

    Add files via upload

---
+++ updatecheck.bat
@@ -0,0 +1,14 @@
+@echo off
+set upver=1.8.9
+set upmc=1.18.2
+set changelogs=사소한 버그 수정, 서버 위치가 어디든지 모든 기능 동작
+set changelink=%링크 추가 계획%
+echo 현재 런처 버전 : %ver%
+echo 현재 마크 버전 : %mcver%
+echo.
+echo 업데이트할 런처 버전 : %upver%
+echo 업데이트할 마크 버전 : %mcver%
+echo.
+echo 체인지로그 :
+echo %changelogs%
+echo 더 많은 정보 : %changelink%
